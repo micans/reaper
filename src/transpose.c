578050c75ac63e07b50bcb8d6c0d506c7b637993
Stijn van Dongen <micans@users.noreply.github.com>
2018-05-21 18:10:32 +0100

    Tidying up and documenting transpose.c

--- src/transpose.c
+++ src/transpose.c
@@ -1,29 +1,30 @@
 
 
-/* features:
-      transpose tabular data.
-      record and field separator can be changed.
-      can read gzipped files.
-      fast as long as data fits into memory.
-      can handle headers lacking dummy lead field.
-*/
+/* Author: Stijn van Dongen
 
+ * features:
+    - transpose tabular data.
+    - record and field separator can be changed.
+    - can read and write gzipped files.
+    - fast as long as data fits into memory.
+    - can handle headers lacking dummy lead field.
+    - in pad mode (--pad-ragged) it will padd the transposed table with empty fields
+      to make it rectangular, should the input have rows with different amount of cells.
+    - In normal operation, a ragged array will be considered an error and lead to termination.
 
-/* todo
-   -  encapsulate
+ * TODO
+    - encapsulate
        - input reading
-       - add_fake_tab offset fiddling
+       - add_fake_tab offset fiddling. data[1] g_nl
        - removed_last_newline logic
-   #  account for missing tab in header line .... (count mismatch)
-   #  warn for count mismatches / output counts
-   -  in check mode do not read in file in entirety.
-   /  optionally do not print out trailing nl
-   #  gzread
-   ?  mmap file
-   -  write row, col labels
-   /  test with empty lines (rec_offset stress-test)
-   /  test with all-empty fields
-   -  optify na/nan/-/empty output fields
+    ? Open output file only if tests are passed.
+    # account for missing tab in header line .... (count mismatch)
+    # warn for count mismatches / output counts
+    # gzread
+    ? write row, col labels
+    / test with empty lines (rec_offset stress-test)
+    / test with all-empty fields
+    ? optify na/nan/-/empty output fields
 */
 
 #include <stdio.h>
@@ -39,18 +40,20 @@ unsigned char g_tab = '\t';
 unsigned char g_nl  = '\n';
 int removed_last_newline = 0;
 
-int g_debug = 0, g_check = 0;
+int g_debug = 0, g_check = 0, g_strict = 1;
 
 
 
 struct offset
-{  unsigned long this
-;  unsigned long next
+{  unsigned long this            /* when writing a cell, this is where we start to write */
+;  unsigned long next            /* and this is where it ends. Next loop this is set to next */
 ;  unsigned long n_sep
 ;
 }  ;
 
 
+      /* Returns 0 if we need to add a fake tab, 1 otherwise
+      */
 int field_check_ok
 (  unsigned char* data
 ,  unsigned long N
@@ -65,32 +68,40 @@ int field_check_ok
       ;  while (p < z && (p = memchr(p, g_tab, offsets[i].next - offsets[i].this - (p-o))))
             offsets[i].n_sep++
          ,  p++
-      ;  if (!g_check && i == 3)
+;if (g_debug)
+fprintf(stderr, "line %lu this %lu next %lu nsep %lu\n", i, offsets[i].this, offsets[i].next, offsets[i].n_sep)
+      ;  if (i == 3 && !g_check && !g_strict)
          break
    ;  }
 
-                           /* note: the fake tab was already added (add_fake_tab), so computations
-                            * below look weird: equality means deficit of one,
-                            * number of separators == number of fields for leading record.
-
-                            * in check mode we compare with the *second* record.
+                           /* In check/strict mode we compare with the *second* record.
+                            * The first record may have things going on with the first column
+                            * -- it may be meaningless and hence missing. Additionally,
+                            * the separator may be mising as well.
                            */
-      if (g_check)
-      {  unsigned long n_nok = 0
-      ;  if (n_offsets > 1)
-         arrr("second record has %lu fields", (offsets[1].n_sep))
+      {  unsigned long n_nok = 0, reference = offsets[1].n_sep
+      ;  if (g_check && n_offsets > 1)
+         arrr("Second record has %lu fields", (offsets[1].n_sep+1))
       ;  for (i=2;i<n_offsets-1;i++)
-         if (offsets[i].n_sep != offsets[1].n_sep)
-            fprintf(stderr, " %lu/%lu", (i+1), offsets[i].n_sep)
+         if (offsets[i].n_sep != reference)
+            fprintf(stderr, " %lu/%lu", (i+1), (offsets[i].n_sep+1))
          ,  n_nok++
       ;  if (n_nok)
-         fputc('\n', stderr)
-      ;  else
-         arrr("file is clear")
-      ;  exit(n_nok ? 1 : 0)
+         fprintf(stderr, " (line no/field count)\n")
+      ;  if (g_check)
+         exit(n_nok ? 1 : 0)
+      ;  else if (g_strict && n_nok)
+            arrr("Count discrepancies found, used second line as reference with %lu fields", reference+1)
+         ,  exit(1)
    ;  }
 
-      else if (i >= 2 && offsets[0].n_sep == offsets[1].n_sep)
+                     /* Below comparison looks crazy. That's because we also counted the
+                      * fake tab that exists as a precaution. It was easiest to do that the
+                      * way it is done, and accept this bizarre comparison -- bizarre and crazy
+                      * because if the identity is true we have in fact an off-by-one situation,
+                      * indicating a header line with one fewer element than the second line.
+                     */
+      if (i >= 2 && offsets[0].n_sep == offsets[1].n_sep)
          arrr
          (  "first and second line have %lu and %lu separators; I'll add a leading separator"
          ,  offsets[0].n_sep-1
@@ -98,14 +109,11 @@ int field_check_ok
          )
       ,  ret = 0
 
-   ;  else if (i >= 2 && offsets[0].n_sep-1 != offsets[1].n_sep)
-      arrr("first and second line have %lu and %lu separators; you probably know what your are doing", offsets[0].n_sep-1, offsets[1].n_sep)
-
    ;  return ret
 ;  }
 
 
-void transpose
+void do_transpose
 (  void* fpo
 ,  unsigned char* data
 ,  unsigned long N
@@ -115,11 +123,11 @@ void transpose
 )
    {  int n_active
    ;  if (field_check_ok(data, N, offsets, n_offsets))
-         offsets[0].this = 1                /* this is tied to add_fake_tab */
-      ,  data[1] = g_nl
+         offsets[0].this = 1       /* No need to parse the fake tab */
+      ,  data[1] = g_nl            /* Not meaningful; still need to test this however. */
    ;  do 
       {  unsigned long i
-      ;  n_active = 0
+      ;  n_active = 0           /* this keeps track whether an unfinished input column still exists */
       ;  for (i=0; i<n_offsets-1;i++)
          {  unsigned long o = offsets[i].this
          ;  unsigned remaining = offsets[i].next - offsets[i].this
@@ -157,6 +165,7 @@ void transpose
       ;  }
       }
       while (n_active > 0)
+
    ;  if (removed_last_newline) {
 #if WE_USE_ZLIB
          if (zippit)
@@ -180,7 +189,6 @@ int main
    ;  unsigned long N = 0, n_offsets = 0, n_offsets_alloc = 1<<10
    ;  long n = 0
    ;  struct offset *offsets = myalloc(n_offsets_alloc * sizeof offsets[0])
-   ;  int i
    ;  int zippit = 1
    ;  const char* g_fnin = "-"
    ;  const char* g_fnout = "-"
@@ -199,13 +207,15 @@ int main
       uniarg("--nozip")  zippit = 0;  endarg()
 #endif
       uniarg("--check")  g_check = 1; endarg()
+      uniarg("--pad-ragged") g_strict = 0; endarg()
    uniarg("-h")
 puts("-i <fname>        input stream (gzipped file allowed)");
 puts("-o <fname>        output stream (gzipped file allowed)");
 puts("-t <CHAR>         field delimiter (default TAB)");
 puts("-n <CHAR>         record delimiter (default NEWLINE)");
 puts("--debug           output record offset information");
-puts("--check           check field count consistency");
+puts("--check           check table consistency (rectangulosity) and exit");
+puts("--pad-ragged      allow ragged table input, pad output");
 #if WE_USE_ZLIB
 puts("--nozip           do not gzip the output");
 #endif
@@ -224,10 +234,14 @@ exit(0);
 
    ;  offsets[0].this = 0
    ;  offsets[0].next = -1
+   ;  offsets[0].n_sep = 0
    ;  n_offsets = 1
 
-   ;  data[N++] = g_nl
+   ;  data[N++] = g_nl   /* should have documented this .. we don't seem to memchr it */
    ;  data[N++] = g_tab  /* in case we need to add one when header line is one short: add_fake_tab */
+                         /* This *is* memchr'ed by the field_check_ok code; hence we get a
+                          * discrepancy for the first line.
+                         */
 
 #  if WE_USE_ZLIB
    ;  while ((n = gzread(input, data+N, BUFSIZE)) > 0)
@@ -275,13 +289,9 @@ exit(0);
 
       offsets[n_offsets-1].next = -1
 
-;if(g_debug)
- for (i=0;i<n_offsets;i++)
- arrr("%d record %d - %d", (int) n, (int) offsets[i].this, offsets[i].next)
-
 ;if(0)fprintf(stdout, "[%s], %d offsets\n", data, (int) n_offsets)
 
-   ;  transpose(fpo, data, N, offsets, n_offsets, zippit)
+   ;  do_transpose(fpo, data, N, offsets, n_offsets, zippit)
    ;  myfzclose(fpo, zippit)
    ;  free(data)
    ;  free(offsets)
